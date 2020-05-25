0ec4922975501396fa67c97f7bc11283b20ddd02
dbmiddle <dbmiddle39@gmail.com>
2020-05-25 13:31:36 -0400

    fixed edit url routes, added some styling to help with legibility

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -41,10 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tracker.apps.TrackerConfig',
-    'crispy_forms'
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
